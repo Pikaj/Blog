6b4a75046ba2a64224e875efe70c0fda0b0d9e96
Pikaj <pikajpij@gmail.com>
2013-11-10 21:58:48 +0100

    serwis naprawiony

--- app/services/CreatePostService.rb
+++ app/services/CreatePostService.rb
@@ -2,16 +2,10 @@ class CreatePostService
   class EmptyTitleError < StandardError; end
   class EmptyPostError < StandardError; end
   class NoCategoryError< StandardError; end
-  class PostError < StandardError; end
 
   def process(author, params)
     raise EmptyTitleError.new unless !params[:title].empty?
     raise EmptyPostError.new unless !params[:text].empty?
     raise NoCategoryError.new unless !params[:category_id].empty?
-
-    post = Post.new(params.permit(:title,:text,:category_id))
-    post.user = author
-    render 'new' if !post.save
-    raise PostError.new unless post.save
   end 
 end
